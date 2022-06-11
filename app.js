d63661459eda9710e6c216dcf253eb9aa5443ea8
SeijiGabriel <104286173+SeijiGabriel@users.noreply.github.com>
2022-06-12 04:13:07 +0900

    Voltando no tempo

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
-// mais um console log de teste:
-console.log("O Gabriel fez esse commit porque esta sozinho!")
-console.log("Enviando para o GitHub")
 
